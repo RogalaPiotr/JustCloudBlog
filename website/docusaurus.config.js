585e851a146ca328cb06344840bb031351778aa1
Piotr Rogala <71rogi@gmail.com>
2022-01-23 16:44:14 +0100

    test gh actions

---
+++ website/docusaurus.config.js
@@ -0,0 +1,130 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'JustCloud Blog',
+  tagline: 'JustCloud Blog',
+  url: 'https://blog.justcloud.pl',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'https://raw.githubusercontent.com/RogalaPiotr/JustCloudWeb/main/assets/images/favicon-new.ico',
+  organizationName: 'RogalaPiotr', // Usually your GitHub org/user name.
+  projectName: 'JustCloudBlog', // Usually your repo name.
+  trailingSlash: false,
+
+  plugins: [
+    // ... Your other plugins.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+        language: ["en"],
+        highlightSearchTermsOnTargetPage: true,
+        indexDocs: false,
+        blogRouteBasePath: "/"
+      },
+    ],
+  ],
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          editLocalizedFiles: false,
+          routeBasePath: '/'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'JustCloud.pl',
+        logo: {
+          alt: 'JustCloud.pl',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://justcloud.pl/index.html#form5-28',
+            label: 'Contact',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/RogalaPiotr/justcloudpublic',
+            label: 'GitHub',
+            position: 'right',
+          }
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'JustCloud links',
+            items: [
+              {
+                label: 'https://justcloud.pl',
+                to: 'https://justcloud.pl',
+              },
+              {
+                label: 'Contact',
+                to: 'https://web.justcloud.pl/index.html#form5-28',
+              },
+              {
+                label: 'Github',
+                to: 'https://github.com/RogalaPiotr/justcloudpublic',
+              },
+              {
+                label: 'Azure DevOps',
+                to: 'https://dev.azure.com/justcloudpublic',
+              }
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Meetup',
+                href: 'https://www.meetup.com/Microsoft-Azure-Users-Group-Poland/',
+              }
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/RogalaPiotr/justcloudpublic',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} JustCloud.pl, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;

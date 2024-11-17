3538a3302828e5e97056ed5faa1e42956c89069f
Piotr Rogala <71rogi@gmail.com>
2024-11-17 13:24:33 +0100

    remove locale

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -30,15 +30,6 @@ const config: Config = {
     require.resolve('docusaurus-plugin-image-zoom')
   ],
 
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en','pl'],
-  },
-
   presets: [
     [
       'classic',

c0c631091458a6bc6905ac85f572dd2789dafc51
Piotr Rogala <71rogi@gmail.com>
2025-10-26 12:11:18 +0100

    fix: Update default locale to Polish in Docusaurus configuration

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -111,8 +111,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'pl',
+    locales: ['pl'],
   },
 
   // SEO: Structured data and JSON-LD

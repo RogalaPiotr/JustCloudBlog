2dccd041c5cfb61e990527e6da0b1464e725ce67
Piotr Rogala <71rogi@gmail.com>
2025-10-26 20:57:06 +0100

    feat: Enhance BlogListPage schema; add blog postings with metadata for improved SEO and structured data compliance

--- website/src/theme/BlogListPage/index.js
+++ website/src/theme/BlogListPage/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import BlogListPage from '@theme-original/BlogListPage';
 import Head from '@docusaurus/Head';
+import { useBlogListContext } from '@docusaurus/plugin-content-blog/client';
 
 export default function BlogListPageWrapper(props) {
     const siteUrl = 'https://blog.justcloud.pl';
+
+    // Get blog posts from context
+    const { items } = useBlogListContext();
 
     // Bing SEO requirement: description must be 25-160 characters
     const description = "Eksperckie artykuły techniczne o Microsoft Azure, Azure DevOps, automatyzacji, governance, security i best practices dla cloud transformation";
@@ -11,7 +15,31 @@ export default function BlogListPageWrapper(props) {
     // Image for social sharing (same as in BlogPostSEO.js)
     const imageUrl = `${siteUrl}/img/justcloud-social-card.png`;
 
-    // Blog schema for list page (homepage) with image field
+    // Generate BlogPosting list from current page items
+    const blogPostings = items.map(({ content }) => {
+        const { metadata } = content;
+        const postUrl = `${siteUrl}${metadata.permalink}`;
+        const author = metadata.authors?.[0];
+
+        return {
+            "@type": "BlogPosting",
+            "@id": postUrl,
+            "headline": metadata.title,
+            "name": metadata.title,
+            "description": metadata.description,
+            "url": postUrl,
+            "datePublished": metadata.date,
+            "dateModified": metadata.frontMatter.last_update?.date || metadata.date,
+            "image": "https://blog.justcloud.pl/img/logo.png",
+            "author": {
+                "@type": "Person",
+                "name": author?.name || "Piotr Rogala",
+                "url": author?.url || "https://github.com/RogalaPiotr"
+            }
+        };
+    });
+
+    // Blog schema for list page (homepage) with blogPost field
     const blogSchema = {
         "@context": "https://schema.org",
         "@type": "Blog",
@@ -45,7 +73,9 @@ export default function BlogListPageWrapper(props) {
                 "https://twitter.com/RogalaPiotr",
                 "https://github.com/RogalaPiotr"
             ]
-        }
+        },
+        // Add list of BlogPosting items
+        "blogPost": blogPostings
     };
 
     return (

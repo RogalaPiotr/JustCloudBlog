9395a9fdae8847ed102c7690e6b62cc4154637ff
Piotr Rogala <71rogi@gmail.com>
2025-10-26 21:45:27 +0100

    feat: Add hidden H1 for Bing SEO compliance; improve accessibility for screen readers

--- website/src/theme/BlogListPage/index.js
+++ website/src/theme/BlogListPage/index.js
@@ -76,6 +76,18 @@ export default function BlogListPageWrapper(props) {
           {JSON.stringify(blogListSchema)}
         </script>
       </Head>
+      
+      {/* SEO: Hidden H1 for Bing (screen reader accessible, visually hidden) */}
+      <h1 style={{
+        position: 'absolute',
+        left: '-9999px',
+        width: '1px',
+        height: '1px',
+        overflow: 'hidden'
+      }}>
+        JustCloud.pl Blog - Azure, DevOps & Cloud Engineering
+      </h1>
+      
       <BlogListPage {...props} />
     </>
   );
